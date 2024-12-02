255d2cf7ec757190a7d74e9db4c47ee6c92005cd
Daniel Romero <danielromero.sem@gmail.com>
2024-12-02 05:28:42 +0100

    Add base, 404 html

--- BoosterSimulator/vite.config.ts
+++ BoosterSimulator/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/PokemonPocketPackSimulator/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
